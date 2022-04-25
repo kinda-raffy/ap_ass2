30e1ce2bd51d6f1be376c274d15cad32786f1514
kinda_raffy <rafat.mahiuddin@gmail.com>
2022-04-25 22:05:44 +1000

    Implemented 2.1 and 2.2

--- scrabble.cpp
+++ scrabble.cpp
@@ -1,15 +1,127 @@
-
 #include "LinkedList.h"
 
 #include <iostream>
+#include <algorithm>
+#include <fstream>
 
 #define EXIT_SUCCESS    0
+#define EXIT_FAILURE    1
 
-int main(void) {
-   LinkedList* list = new LinkedList();
-   delete list;
 
-   std::cout << "TODO: Implement Scrabble!" << std::endl;
+int credits() {
+    std::cout << "----------------------------------\n";
+    std::cout << "Name: Rafat Mahiuddin\n";
+    std::cout << "Student ID: s3897093\n";
+    std::cout << "Email: s3897093@student.rmit.edu.au\n\n";
 
-   return EXIT_SUCCESS;
+    std::cout << "Name: Peppa Pig\n";
+    std::cout << "Student ID: s3897093\n";
+    std::cout << "Email: s3897093@student.rmit.edu.au\n\n";
+
+    std::cout << "Name: George .\n";
+    std::cout << "Student ID: s3897093\n";
+    std::cout << "Email: s3897093@student.rmit.edu.au\n\n";
+
+    std::cout << "Name: Mummy Pig\n";
+    std::cout << "Student ID: s3897093\n";
+    std::cout << "Email: s3897093@student.rmit.edu.au\n\n" << std::endl;
+    return EXIT_SUCCESS;
+}
+
+
+int validFile(const std::string& fileName) {
+    int returnStatus = EXIT_SUCCESS;
+    std::ifstream file(fileName);
+    if (!file.is_open()) {
+        std::cout << "File " << fileName << " not found" << std::endl;
+        returnStatus = EXIT_FAILURE;
+    }
+    file.close();
+    // TODO - check file format
+    return returnStatus;
+}
+
+
+int loadGame() {
+    std::cout << "Enter the filename from which load a game" << std::endl;
+    std::string filename;
+    std::cin >> filename;
+    if (validFile(filename)) {
+        std::cout << "Scrabble game successfully loaded" << std::endl;
+        // TODO - load game
+    }
+    return EXIT_SUCCESS;
+}
+
+
+
+std::string getPlayerName(int num) {
+    std::string playerName;
+    std::cout << "Enter a name for player " << num << " (uppercase characters only)" << std::endl;
+    std::cin >> playerName;
+    while (!(std::all_of(playerName.begin(), playerName.end(), isupper))) {
+        std::cout << "Invalid name. Please enter a name for player " << num << " (uppercase characters only)" << std::endl;
+        std::cin >> playerName;
+    }
+    return playerName;
+}
+
+
+int newGame() {
+    std::cout << "Starting a New Game" << std::endl;
+    std::string player1Name = getPlayerName(1);
+    std::string player2Name = getPlayerName(2);
+    std::cout << "Let's Play" << std::endl;
+    // TODO - create a new game
+    std::cout << "Player 1: " << player1Name << std::endl;
+    std::cout << "Player 2: " << player2Name << std::endl;
+    return EXIT_SUCCESS;
+}
+
+
+int menu() {
+    int choice;
+    std::cout << "1. New Game\n";
+    std::cout << "2. Load Game\n";
+    std::cout << "3. Credits (Show student information)\n";
+    std::cout << "4. Quit\n";
+    std::cin >> choice;
+    return choice;
+}
+
+
+void menuHandler() {
+    int choice = menu();
+    if (choice == 1) {
+        newGame();
+    } else if (choice == 2) {
+        loadGame();
+    } else if (choice == 3) {
+        credits();
+        // Re-display menu.
+        menuHandler();  // FIXME - can this lead to errors?
+    } else if (choice == 4) {
+        std::cout << "Goodbye" << std::endl;
+    } else {
+        std::cout << "Invalid choice" << std::endl;
+    }
+}
+
+
+void greet() {
+   std::cout << "Welcome to Scrabble!\n";
+   std::cout << "-------------------" << std::endl;
+}
+
+
+int main() {
+    greet();
+    menuHandler();
+
+    auto* list = new LinkedList();
+    delete list;
+
+    std::cout << "TODO: Implement Scrabble!" << std::endl;
+
+    return EXIT_SUCCESS;
 }
