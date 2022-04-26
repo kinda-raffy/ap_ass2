c9009ed9b463bc4714e414b8619488bf45c7e7db
alrebellato <74402509+alrebellato@users.noreply.github.com>
2022-04-26 15:48:49 +1000

    Implemented TileValue map

--- Tile.h
+++ Tile.h
@@ -1,10 +1,19 @@
 #ifndef ASSIGN2_TILE_H
 #define ASSIGN2_TILE_H
 
+#include <map>
+
 class Tile {
 public:
-    char letter;
-    int value;
+   Tile(char);
+   char letter;
+   int getValue();
 };
+
+
+
+
+
+
 
 #endif // ASSIGN2_TILE_H
