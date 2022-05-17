ac143aa4a00f1a1200b1e3cbcf1a076be15e337f
VisibleReality <37581230+VisibleReality@users.noreply.github.com>
2022-05-17 22:59:28 +1000

    Add validity checks to tile constructor and setletter

--- Tile.cpp
+++ Tile.cpp
@@ -2,55 +2,63 @@
 
 // Values constant for each letter. Dash character represents null tiles.
 const static std::map<char, int> values = {
-	{'-', 0}, 
-	{'A', 1}, 
-	{'B', 3},
-	{'C', 3},
-	{'D', 2},
-	{'E', 1},
-	{'F', 4},
-	{'G', 2},
-	{'H', 4},
-	{'I', 1},
-	{'J', 8},
-	{'K', 5},
-	{'L', 1},
-	{'M', 3},
-	{'N', 1},
-	{'O', 1},
-	{'P', 3},
-	{'Q', 10},
-	{'R', 1},
-	{'S', 1},
-	{'T', 1},
-	{'U', 1},
-	{'V', 4},
-	{'W', 4},
-	{'X', 8},
-	{'Y', 4},
-	{'Z', 10},
+        {'-', 0},
+        {'A', 1},
+        {'B', 3},
+        {'C', 3},
+        {'D', 2},
+        {'E', 1},
+        {'F', 4},
+        {'G', 2},
+        {'H', 4},
+        {'I', 1},
+        {'J', 8},
+        {'K', 5},
+        {'L', 1},
+        {'M', 3},
+        {'N', 1},
+        {'O', 1},
+        {'P', 3},
+        {'Q', 10},
+        {'R', 1},
+        {'S', 1},
+        {'T', 1},
+        {'U', 1},
+        {'V', 4},
+        {'W', 4},
+        {'X', 8},
+        {'Y', 4},
+        {'Z', 10},
 };
 
 Tile::Tile(char letter)
-	: letter {letter} {
+        : letter{letter} {
+    if (values.find(letter) == values.end())
+    {
+        throw std::runtime_error("Invalid Letter");
+    }
 }
 
 Tile::Tile(const Tile &source)
-	: Tile {source.letter} {
+        : Tile{source.letter} {
 }
 
 char Tile::getLetter() const {
-	return letter;
+    return letter;
 }
 
 void Tile::setLetter(char letter) {
-	this->letter = letter;
+    if (values.find(letter) == values.end())
+    {
+        throw std::runtime_error("Invalid Letter");
+    }
+    this->letter = letter;
 }
 
 int Tile::getValue() const {
-	return values.at(letter);
+    return values.at(letter);
 }
 
 int Tile::getTileValue(char letter) {
-	return values.at(letter);
+    return values.at(letter);
 }
