1f5bd562a60878710aa587e5463251276855bd7e
VisibleReality <37581230+VisibleReality@users.noreply.github.com>
2022-05-17 23:14:04 +1000

    Docstrings for Tile.h

--- Tile.h
+++ Tile.h
@@ -6,19 +6,55 @@
 
 class Tile {
 
-   char letter;
+    char letter;
 
 public:
 
-   explicit Tile(char);
-   Tile(const Tile&);
-   ~Tile() = default;
+    /**
+     * @brief Create a tile with the given letter.
+     *
+     * @param letter The letter this tile will contain.
+     */
+    explicit Tile(char);
 
-   int getValue() const;
-   char getLetter() const;
-   void setLetter(char);
+    /**
+     * @brief Create a copy of the given tile.
+     *
+     * @param tile The tile to create a copy of.
+     */
+    Tile(const Tile &);
 
-   static int getTileValue(char);
+    ~Tile() = default;
+
+    /**
+     * @brief Get the point value of the tile.
+     *
+     * @return The point value associated with the letter of this tile.
+     */
+    int getValue() const;
+
+    /**
+     * @brief Get the letter of this tile.
+     *
+     * @return A char representing the letter of this tile.
+     */
+    char getLetter() const;
+
+    /**
+     * @brief Set the letter of this tile.
+     * Throws exception if letter is invalid.
+     *
+     * @param letter The char to set.
+     */
+    void setLetter(char);
+
+    /**
+     * @brief Gets the point value of the letter passed in.
+     *
+     * @param letter The letter to get a point value of.
+     * @return The point value of letter in scrabble.
+     */
+    static int getTileValue(char);
 };
 
 #endif // ASSIGN2_TILE_H
